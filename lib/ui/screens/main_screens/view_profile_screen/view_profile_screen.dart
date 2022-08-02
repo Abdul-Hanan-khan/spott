fb526bab9f103a5b6c116676338e381e7e2a1a1c
Abdul Hanan <81586087+Abdul-Hanan-khan@users.noreply.github.com>
2022-08-02 08:01:32 -0700

    translations and other few issues resolved

--- lib/ui/screens/main_screens/view_profile_screen/view_profile_screen.dart
+++ lib/ui/screens/main_screens/view_profile_screen/view_profile_screen.dart
@@ -391,6 +391,7 @@ class _ViewProfileScreenState extends State<ViewProfileScreen>
                   textAlign: TextAlign.center,
                 ),
               ),
+
             ],
           ),
         ),
