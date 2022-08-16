c8dd795de668b4f48ee1603f77fa4c06e910a27e
AbdulHanan <hanankhanajk@gmail.com>
2022-08-16 22:57:56 +0500

    chat functionality fixed completely

--- lib/resources/api_providers/login_api_provider.dart
+++ lib/resources/api_providers/login_api_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:spott/models/api_responses/login_api_response.dart';
 import 'package:spott/utils/constants/api_constants.dart';
@@ -12,7 +13,16 @@ class LoginApiProvider {
       final dio = Dio();
       final Response response = await dio
           .post(ApiConstants.baseUrl + ApiConstants.loginUrl, data: _data);
+
       if (response.statusCode == 200) {
+        String userId;
+        userId = (response.data)["data"]['user']["id"].toString();
+
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(userId.toString())
+            .set({'userState': 0, 'isNewMessage': 0});
+
         return LoginApiResponse.fromJson(response.data);
       } else {
         return LoginApiResponse(message: response.toString());
